283bffafcb970f5a2b4a6d7b8e70b4c0f9d8e25d
LicFuraca <martinexequield@gmail.com>
2023-12-29 15:35:56 -0300

    Subo router para el registro, el template del registro y la funcionalidad del mismo. (además creo el modelo de mongoDb del mismo)

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
 
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
@@ -11,15 +13,13 @@ const ExpressError = require('./utils/ExpressError')
 
 const campgroundsRouter = require('./routes/campgrounds')
 const reviewsRouter = require('./routes/reviews')
+const userRouter = require('./routes/users')
+const User = require('./models/user')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-
-app.use(methodOverride('_method'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
 
 const oneWeek = 1000 * 60 * 60 * 24 * 7
 const sessionConfig = {
@@ -32,8 +32,18 @@ const sessionConfig = {
         maxAge: oneWeek,
     },
 }
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
@@ -51,6 +61,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use('/campgrounds', campgroundsRouter)
 app.use('/campgrounds/:id/reviews', reviewsRouter)
+app.use('/', userRouter)
 
 // HOME
 app.get('/', (req, res) => {
